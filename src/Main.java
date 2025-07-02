89abbc6d906604b7be9e2cc0325e9517a3fe3db1
ironman3580 <navghadejaydeep@gmail.com>
2025-07-02 17:04:01 +0530

    print string

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main{
     }
     public static void loop(){
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.println("hello sire");
         }
     }
 }
